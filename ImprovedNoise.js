d7f2879daf4c20477ac92f5ca5963fd7d21481e8
James Dunn <jwdunn1@gmail.com>
2018-09-01 20:41:13 -0700

    Add seed method

--- ImprovedNoise.js
+++ ImprovedNoise.js
@@ -1,51 +1,50 @@
-// http://mrl.nyu.edu/~perlin/noise/
+// http://mrl.nyu.edu/~perlin/noise
 // forked from: https://github.com/mrdoob/three.js/blob/master/examples/js/ImprovedNoise.js
-
-var ImprovedNoise = function () {
-
-  var p = [];
-  for (var i = 0; 256 > i ; i ++) p[i] = i;
-  for (var i = p.length - 1; 0 < i; i--) { // Durstenfeld shuffle
-    var j = Math.floor(Math.random() * (i + 1)), t = p[i];
-    p[i] = p[j]; p[j] = t;
+// random function from: jsfiddle.net/Llorx/z2y097ad
+var perlin = function(fl) {
+  function t(a) {
+    return a * a * a * (a * (6 * a - 15) + 10)
   }
-  for (f = 0; 256 > f; f++) p[256 + f] = p[f];
-
-	function fade(t) {
-		return t * t * t * (t * (t * 6 - 15) + 10);
-	}
-
-	function lerp(t, a, b) {
-		return a + t * (b - a);
-	}
-
-	function grad(hash, x, y, z) {
-		var h = hash & 15;
-		var u = h < 8 ? x : y, v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-		return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
-	}
-
-	return {
-		noise: function (x, y, z) {
-			var floorX = Math.floor(x), floorY = Math.floor(y), floorZ = Math.floor(z);
-			var X = floorX & 255, Y = floorY & 255, Z = floorZ & 255;
-
-			x -= floorX;
-			y -= floorY;
-			z -= floorZ;
-
-			var xMinus1 = x - 1, yMinus1 = y - 1, zMinus1 = z - 1;
-			var u = fade(x), v = fade(y), w = fade(z);
-			var A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z, B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;
-
-			return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z),
-							grad(p[BA], xMinus1, y, z)),
-						lerp(u, grad(p[AB], x, yMinus1, z),
-							grad(p[BB], xMinus1, yMinus1, z))),
-					lerp(v, lerp(u, grad(p[AA + 1], x, y, zMinus1),
-							grad(p[BA + 1], xMinus1, y, z - 1)),
-						lerp(u, grad(p[AB + 1], x, yMinus1, zMinus1),
-							grad(p[BB + 1], xMinus1, yMinus1, zMinus1))));
-		}
-	}
-};
+  function h(a, c, b) {
+    return c + a * (b - c)
+  }
+  function g(a, b, d, e) {
+      a &= 15;
+      var c = 8 > a ? b : d;
+      b = 4 > a ? d : 12 == a || 14 == a ? b : e;
+      return (0 == (a & 1) ? c : -c) + (0 == (a & 2) ? b : -b)
+  }
+  var b = [], sd;
+  function random(){do{var b=1E4*Math.sin(sd++);
+    b-=fl(b)}while(.15>b||.9<b);
+    return(b-.15)/.75;
+  }
+  var o = {
+    seed: function(s) {
+      var r = (s === undefined?Math.random:(sd=s,random));
+      for (var i = 0; 256 > i ; i ++) b[i] = i;
+      for (i = 255; 0 < i; i--) {
+        var j = fl(r() * (i + 1)), z = b[i];
+        b[i] = b[j]; b[j] = z;
+      }
+      for (var f = 0; 256 > f; f++) b[256 + f] = b[f];
+    },
+    noise: function(a, c, d) {
+      var e = fl(a),
+          k = fl(c),
+          m = fl(d),
+          n = e & 255, p = k & 255, l = m & 255;
+      a -= e; c -= k; d -= m;
+      e = a - 1; k = c - 1; m = d - 1;
+      var f = t(a), w = t(c), q = b[n] + p, x = b[q] + l;
+      q = b[q + 1] + l;
+      p = b[n + 1] + p;
+      n = b[p] + l;
+      l = b[p + 1] + l;
+      return h(t(d), h(w, h(f, g(b[x], a, c, d), g(b[n], e, c, d)), h(f, g(b[q], a, k, d), g(b[l], e, k, d))), h(w, h(f, g(b[x + 1], a, c, m), g(b[n + 1], e, c, d - 1)), h(f, g(b[q +
+          1], a, k, m), g(b[l + 1], e, k, m))))
+    }
+  };
+  o.seed();
+  return o;
+}(Math.floor);

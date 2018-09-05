af0ac259bf380e156c423c0c3f75bff8bca3a997
James Dunn <jwdunn1@gmail.com>
2018-09-04 22:20:17 -0700

    Add noiseDetail
    Also rename noiseSeed

--- ImprovedNoise.js
+++ ImprovedNoise.js
@@ -14,13 +14,31 @@ var perlin = function(fl) {
       b = 4 > a ? d : 12 == a || 14 == a ? b : e;
       return (0 == (a & 1) ? c : -c) + (0 == (a & 2) ? b : -b)
   }
-  var b = [], sd;
+  var b = [], sd, octaves=4, decay=0.5;
   function random(){do{var b=1E4*Math.sin(sd++);
     b-=fl(b)}while(.15>b||.9<b);
     return(b-.15)/.75;
   }
+  function fract(a,c,d) {
+    var e = fl(a),
+        k = fl(c),
+        m = fl(d),
+        n = e & 255, p = k & 255, l = m & 255;
+    a -= e; c -= k; d -= m;
+    e = a - 1; k = c - 1; m = d - 1;
+    var f = t(a), w = t(c), q = b[n] + p, x = b[q] + l;
+    q = b[q + 1] + l;
+    p = b[n + 1] + p;
+    n = b[p] + l;
+    l = b[p + 1] + l;
+    return h(t(d), h(w, h(f, g(b[x], a, c, d), g(b[n], e, c, d)), h(f, g(b[q], a, k, d), g(b[l], e, k, d))), h(w, h(f, g(b[x + 1], a, c, m), g(b[n + 1], e, c, d - 1)), h(f, g(b[q +
+        1], a, k, m), g(b[l + 1], e, k, m))))
+  }
   var o = {
-    seed: function(s) {
+    noiseDetail: function(lod,falloff) {
+      octaves = lod, decay = falloff;
+    },
+    noiseSeed: function(s) {
       var r = (s === undefined?Math.random:(sd=s,random));
       for (var i = 0; 256 > i ; i ++) b[i] = i;
       for (i = 255; 0 < i; i--) {
@@ -29,22 +47,19 @@ var perlin = function(fl) {
       }
       for (var f = 0; 256 > f; f++) b[256 + f] = b[f];
     },
-    noise: function(a, c, d) {
-      var e = fl(a),
-          k = fl(c),
-          m = fl(d),
-          n = e & 255, p = k & 255, l = m & 255;
-      a -= e; c -= k; d -= m;
-      e = a - 1; k = c - 1; m = d - 1;
-      var f = t(a), w = t(c), q = b[n] + p, x = b[q] + l;
-      q = b[q + 1] + l;
-      p = b[n + 1] + p;
-      n = b[p] + l;
-      l = b[p + 1] + l;
-      return h(t(d), h(w, h(f, g(b[x], a, c, d), g(b[n], e, c, d)), h(f, g(b[q], a, k, d), g(b[l], e, k, d))), h(w, h(f, g(b[x + 1], a, c, m), g(b[n + 1], e, c, d - 1)), h(f, g(b[q +
-          1], a, k, m), g(b[l + 1], e, k, m))))
+    noise: function(x,y,z) {
+      y = y || 0;
+      z = z || 0;
+      var r = fract(x,y,z); // first pass
+      var f = decay, o=2;
+      for (var i=1; i<octaves; i++) {
+        r += f*fract(x*o,y*o,z*o);
+        o*=2.0;
+        f*=0.5;
+      }
+      return r;
     }
   };
-  o.seed();
+  o.noiseSeed();
   return o;
 }(Math.floor);

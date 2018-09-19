15f86b0c8843d9ee91978e91c87f9dc72c0a7dfb
James Dunn <jwdunn1@gmail.com>
2018-09-19 02:02:52 -0700

    Add files via upload

--- Turbine.js
+++ Turbine.js
@@ -1,30 +1,30 @@
 // See https://www.openprocessing.org/sketch/592194
 var m=0;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	strokeCap(SQUARE);
+  createCanvas(windowWidth, windowHeight);
+  strokeCap(SQUARE);
   noFill();
-	ellipseMode(CENTER);
+  ellipseMode(CENTER);
 }
 
 function draw() {
-	background(238);
-	translate(width/2,height/2);
-	rotate(m/4*PI/180);
+  background(238);
+  translate(width/2,height/2);
+  rotate(m/4*PI/180);
   stroke(0);
-	for (var i=0; i<60; i++) {
-		push();
-			rotate(6*i*PI/180);
-			translate(-170,170);
-			strokeWeight(12);
-			arc(0,0, 400-7,400-7, HALF_PI/4, HALF_PI/2);
-			strokeWeight(7);
-			arc(0,0, 400+5,400+5, 0, HALF_PI/2);
+  for (var i=0; i<60; i++) {
+    push();
+      rotate(6*i*PI/180);
+      translate(-170,170);
+      strokeWeight(12);
+      arc(0,0, 400-7,400-7, HALF_PI/4, HALF_PI/2);
+      strokeWeight(7);
+      arc(0,0, 400+5,400+5, 0, HALF_PI/2);
     pop();
-	}
-	m--;
-	stroke(238);
-	strokeWeight(12);
-	ellipse(0,0, 356,356);
-	ellipse(0,0, 622,622);
+  }
+  m--;
+  stroke(238);
+  strokeWeight(12);
+  ellipse(0,0, 356,356);
+  ellipse(0,0, 622,622);
 }
